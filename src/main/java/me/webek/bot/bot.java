a65f55131359ab335541f7db047203c7ed7bc077
Webek <55356419+Webekk@users.noreply.github.com>
2021-11-14 19:15:47 +0100

    Update bot.java

--- src/main/java/me/webek/bot/bot.java
+++ src/main/java/me/webek/bot/bot.java
@@ -21,6 +21,13 @@ public class bot extends ListenerAdapter {
 
     public static void main(String[]args) throws Exception{
         JDA jda = JDABuilder.createDefault("TOKEN").build();
+        
+        
+        CommandClientBuilder pong = new CommandClientBuilder();
+        pong.setOwnerId("Client ID");
+        pong.setPrefix("$");
+        pong.setHelpWord("helpme");
+        pong.addCommand(new Ping());
 
 
         CommandClientBuilder builder = new CommandClientBuilder();
@@ -28,12 +35,16 @@ public class bot extends ListenerAdapter {
         builder.setPrefix("$");
         builder.setHelpWord("helpme");
         builder.addCommand(new ServerInfo());
+        
+        
 
         CommandClient client = builder.build();
+        CommandClient ping = pong.build();
 
 
         jda.addEventListener(client);
-        ;
+        jda.addEventListener(ping);
+
 
 
     }
